cebbfe9c209c9c2ac453064d40b557bc2968fc7d
HidetakaKojo <hidetaka.kojo@gmail.com>
2016-03-16 10:40:51 +0000

    fix typo

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule LoggerJSONFileBackend.Mixfile do
 
   defp package do
     [
-      mainteners: ["Hidetaka Kojo"],
+      maintainers: ["Hidetaka Kojo"],
       licenses: ["ISC"],
       links: %{"GitHub" => "https://github.com/xflagstudio/elixir_logger_json_file_backend"}
     ]
