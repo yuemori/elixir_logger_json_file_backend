6d5ebc8b5582d99d878dac04f3db61606792a377
Ben Spatafora <benjamin.spatafora@gmail.com>
2017-03-07 14:27:36 -0600

    Fix Elixir 1.4 warnings
    - Add parens to zero-arity functions

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule LoggerJSONFileBackend.Mixfile do
      version: "0.1.4",
      description: "Logger backend that write a json map per line to a file",
      elixir: "~> 1.2",
-     package: package,
-     deps: deps]
+     package: package(),
+     deps: deps()]
   end
 
   def application do
